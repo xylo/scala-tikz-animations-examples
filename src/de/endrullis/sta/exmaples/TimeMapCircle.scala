74f661e096b671b3c17d7ba821b3165b0a355cb6
Stefan Endrullis <stefan@endrullis.de>
2014-09-28 12:06:29 +0200

    added more examples

---
+++ src/de/endrullis/sta/exmaples/TimeMapCircle.scala
@@ -0,0 +1,26 @@
+package de.endrullis.sta.exmaples
+
+import de.endrullis.sta._
+
+/**
+ * Letter "A" flying in a circle by using TimeMaps for simulating sin and cos.
+ * This example shall demonstrate TimeMaps can be used and misused. :)
+ *
+ * @author Stefan Endrullis &lt;stefan@endrullis.de&gt;
+ */
+object TimeMapCircle extends ScalaTikzAni {
+
+	conf.boundingBox = Pos(-2,-2) -> Pos(2,2)
+
+	conf.frameRate = 10
+
+	val x = 0.0 changeTo 1.0 in (TimeMap(rt => math.sin(rt*2*math.Pi)), 10)
+	val y = 0.0 changeTo 1.0 in (TimeMap(rt => math.cos(rt*2*math.Pi)), 10)
+
+	add(vi"\node at ($x,$y) {A};")
+	//equivalent code:
+	//add("\\node at ($x,$y) {A};" << ("x" -> x, "y" -> y))
+
+	animateNotInLine("gen/circle", SlideOptions(frameRate = 30))
+
+}
